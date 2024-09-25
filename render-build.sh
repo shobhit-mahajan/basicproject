ae8b24cd713d830ef10b6ac584cf3568c94ee8cf
Shobhit_mahajan <88932500+shobhit-mahajan@users.noreply.github.com>
2024-09-25 19:07:29 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,11 +1,28 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# This script can be used to set up your environment
 
-# Install missing dependencies for Playwright
-apt-get update && apt-get install -y \
+# Install required packages
+apt-get update
+apt-get install -y \
+    libnss3 \
+    libxss1 \
+    libgconf-2-4 \
+    libgtk-3-0 \
+    libgdk-pixbuf2.0-0 \
+    libasound2 \
+    libatk1.0-0 \
+    libcups2 \
+    libdbus-glib-1-2 \
     libenchant-2-2 \
     libsecret-1-0 \
     libmanette-0.2-0 \
-    libGLESv2-2 \
-    libgstreamer-gl1.0-0 \
-    gstreamer1.0-libav \
-    libgstreamer-plugins-bad1.0-0
+    libgstgl-1.0-0 \
+    libgstcodecparsers-1.0-0 \
+    libgles2 \
+    libglib2.0-0 \
+    libx11-xcb1 \
+    libxcb1 \
+    libxcomposite1 \
+    libxrandr2 \
+    libxi6 \
+    libxtst6
